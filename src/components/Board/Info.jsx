bb7919ccf7cc74781025d059e0bd9201c8b6835f
German Sandoval <gasandov@Germans-MacBook-Air.local>
2021-12-13 13:41:38 -0600

    end game statment

--- src/components/Board/Info.jsx
+++ src/components/Board/Info.jsx
@@ -5,11 +5,12 @@ export default function Info({ ships }) {
     <div className="game-info">
       <p>Sink all ships by clicking on the grid</p>
       <div className="ship-info">
-        {ships.map((ship) => (
-          <div className="ship-item">
-            {ship.coordinates.map((cell) => (
+        {ships.map((ship, idx) => (
+          <div key={`sinked-cell-${idx}`} className="ship-item">
+            {ship.coordinates.map((cell, idy) => (
               <div
-                className={`ship-block${cell.isRevealed ? "revealed" : ""}`}
+                key={`sinked-cell-${idx}-${idy}`}
+                className={`ship-block${cell.isRevealed ? " revealed" : ""}`}
               />
             ))}
           </div>
--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { GameContext } from "../../context/GameContext";
 import Cell from "./Cell";
+import Info from "./Info";
 import {
   initializeShips,
   getShipCellsCount,
@@ -11,12 +12,17 @@ import {
 export default function Board() {
   const boardSize = 10;
 
-  const [boardGame, setBoardGame] = useState([]);
-  const [shipCells, setShipCells] = useState(0);
-  const [ships, setShips] = useState([]);
-
   const { difficulty } = useContext(GameContext);
-  const [turns, setTurns] = difficulty;
+  const [turns] = difficulty;
+
+  const [gameState, setGameState] = useState({
+    gameOver: false,
+    revealedShips: 0,
+    shipCells: 0,
+    turnsRemaining: turns,
+  });
+  const [boardGame, setBoardGame] = useState([]);
+  const [ships, setShips] = useState([]);
 
   useEffect(() => {
     const initBoard = initializeBoardGame(boardSize);
@@ -29,39 +35,90 @@ export default function Board() {
 
     setBoardGame(board);
     setShips(defaultShips);
-    setShipCells(initShipCells);
+    setGameState((prevGameState) => ({
+      ...prevGameState,
+      shipCells: initShipCells,
+    }));
   }, []);
 
   const handleCellClicked = (cell) => {
+    const currentTurns = gameState.turnsRemaining - 1;
+    const revealed = gameState.revealedShips + 1;
+
+    setGameState((prevState) => ({
+      ...prevState,
+      turnsRemaining: currentTurns,
+    }));
+
     if (cell.isSelected) {
       return;
     }
 
-    const board = JSON.parse(JSON.stringify(boardGame));
+    const boardCopy = JSON.parse(JSON.stringify(boardGame));
+    const shipsCopy = ships.slice();
 
-    board[cell.coordinates.x][cell.coordinates.y].isSelected = true;
+    boardCopy[cell.coordinates.x][cell.coordinates.y].isSelected = true;
 
-    setBoardGame(board);
-    setTurns((currentTurns) => currentTurns - 1);
+    if (cell.isShip) {
+      for (let x = 0; x < shipsCopy.length; x++) {
+        const { coordinates } = shipsCopy[x];
+        for (let y = 0; y < coordinates.length; y++) {
+          const shipCell = coordinates[y];
+
+          if (
+            shipCell.x === cell.coordinates.x &&
+            shipCell.y === cell.coordinates.y
+          ) {
+            shipsCopy[x].coordinates[y].isRevealed = true;
+
+            setGameState((prevState) => ({
+              ...prevState,
+              revealedShips: revealed,
+            }));
+
+            break;
+          }
+        }
+      }
+    }
+
+    setBoardGame(boardCopy);
+    setShips(shipsCopy);
+
+    if (currentTurns === 0 || revealed === gameState.shipCells) {
+      setGameState((prevState) => ({
+        ...prevState,
+        gameOver: true,
+      }));
+    }
   };
 
   return (
     <div className="board">
-      <div>Turns remaining: {isFinite(turns) ? turns : "Infinity"}</div>
-      {turns > 0 ? (
-        boardGame.map((row, xIndex) => (
-          <div key={`row-${xIndex}`}>
-            {row.map((cell, yIndex) => (
-              <Cell
-                key={`cell-${xIndex}-${yIndex}`}
-                onCellClicked={handleCellClicked}
-                cell={cell}
-              />
+      {gameState.gameOver && <p>Game Over</p>}
+      {!gameState.gameOver && (
+        <>
+          <div>
+            <div>
+              Turns remaining:{" "}
+              {isFinite(gameState.turnsRemaining)
+                ? gameState.turnsRemaining
+                : "Infinity"}
+            </div>
+            {boardGame.map((row, xIndex) => (
+              <div key={`row-${xIndex}`}>
+                {row.map((cell, yIndex) => (
+                  <Cell
+                    key={`cell-${xIndex}-${yIndex}`}
+                    onCellClicked={handleCellClicked}
+                    cell={cell}
+                  />
+                ))}
+              </div>
             ))}
           </div>
-        ))
-      ) : (
-        <p>Game Over</p>
+          <Info ships={ships} />
+        </>
       )}
     </div>
   );
